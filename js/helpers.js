896e5dd1a57a14925eb67e1b98ea9a67432b9ed6
GuillermoA231 <andradajoaquin302@gmail.com>
2023-05-30 13:20:20 -0300

    modifica ancho imagenes del collage

--- js/helpers.js
+++ js/helpers.js
@@ -1,21 +1,53 @@
 
+const descripcion = document.getElementById("descripcion");
+const maxLength = parseInt(descripcion.getAttribute('maxlength'));
+const fechaInput = document.getElementById("fecha");
+const areaProgramatica = document.getElementById("areaProgramatica");
+const areaOperativa = document.getElementById("areaOperativa");
 
-function validarCantidadCaracteres(text, min, max) {
-  console.log("text helpers: " + text);
-  if (text.length >= min && text.length <= max) {
-    console.log("Texto Valido");
-    return true;
-  } else {
-    console.log("Texto Invalido");
+function validarFecha() {
+  const fechaIngresada = new Date(fechaInput.value);
+  const fechaActual = new Date();
+
+  fechaActual.setHours(0, 0, 0, 0); // Establecer la hora actual a las 00:00:00 para comparar solo las fechas
+
+  if (fechaIngresada > fechaActual) {
     return false;
   }
+  return true;
 }
 
-export function resumenValidaciones(titulo) {
+function validarAreaOperativa() {
+
+  if (areaProgramatica.value === "" && areaOperativa.value !== "") {
+    return false;
+  }
+  return true;
+}
+function validarDescripcion() {
+  
+  if (descripcion.value.length > maxLength) {
+    descripcion.value = descripcion.value.substring(0, maxLength);
+  }
+  
+  return true;
+}
+
+
+export function resumenValidaciones() {
+  const fechaInput = document.getElementById("fecha");
+  const areaOperativaSelect = document.getElementById("areaOperativa");
+  const descripcionTextarea = document.getElementById("descripcion");
+
   let resumen = "";
 
-  if (!validarCantidadCaracteres(titulo, 2, 100)) {
-    resumen = "El titulo debe contener entre 2 y 100 caracteres";
+  if (!validarFecha(fechaInput)) {
+    resumen = "La fecha no debe ser posterior a la fecha actual.";
+  } else if (!validarAreaOperativa()) {
+    resumen = "Debe seleccionar primero un área programática.";
+  } else if (!validarDescripcion()) {
+    resumen = "La descripción no puede superar los 300 caracteres.";
   }
+
   return resumen;
 }
